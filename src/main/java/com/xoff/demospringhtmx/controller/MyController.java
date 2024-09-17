b4708644b04c6bf7eb2a33ad1f2be28ecbde46a3
xoff69 <xoff.pichon@gmail.com>
2024-09-16 20:48:53 -0600

    clear form input after submit

--- src/main/java/com/xoff/demospringhtmx/controller/MyController.java
+++ src/main/java/com/xoff/demospringhtmx/controller/MyController.java
@@ -32,28 +32,12 @@ public class MyController {
     System.out.println("todoItemRepository "+todoItemRepository);
     return "index";
   }
-  @PostMapping("/todos/{id}/complete")
-  public String completeTodo(@PathVariable Integer id, Model model) {
-    TodoEntity item = null;
-    for (TodoEntity existingItem :  todoItemRepository.findAll()) {
-      if (existingItem.getId()==id) {
-        item = existingItem;
-        break; 
-      }
-    }
-    if (item != null) {
-      item.setCompleted(!item.isCompleted());
-    }
-    model.addAttribute("item",item);
-    return "todo"; 
-  }
+
   @PostMapping("/todos")
   public String createTodo(Model model, @ModelAttribute TodoEntity newTodo) {
-    List<TodoEntity> items=makeCollection(todoItemRepository.findAll());
-    int nextId = items.stream().mapToInt(TodoEntity::getId).max().orElse(0) + 1;
-    newTodo.setId(nextId);
     System.out.println("newTodo: " + newTodo);
     todoItemRepository.save(newTodo);
+
     model.addAttribute("item", newTodo);
     return "todo";
   }
